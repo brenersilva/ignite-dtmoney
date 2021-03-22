f980a654aa573dc75476c2f049332a01108d2eea
brenersilva <brenersrosa@gmail.com>
2021-03-22 19:45:58 -0300

    :fire: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>D3V</h1>
+    <>
       <GlobalStyle />
-    </div>
+      <Header />
+    </>
   );
 }
