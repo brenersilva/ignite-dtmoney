e59ba9faf988287a3faa38de78b761216b0e4293
brenersilva <brenersrosa@gmail.com>
2021-03-23 19:23:28 -0300

    :fire: Modal form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
 
+import closeImg from '../../assets/close.svg';
+
+import { Container } from './styles';
+
 Modal.setAppElement('#root');
 
 interface NewTransactionModalProps {
@@ -12,8 +16,36 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Hello</h2>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modal"/>
+      </button>
+
+      <Container>
+        <h2>Register transaction</h2>
+        
+        <input
+          placeholder="Title"
+        />
+
+        <input
+          type="number"
+          placeholder="Value"
+        />
+
+        <input
+          placeholder="Category"
+        />
+
+        <button type="submit">Register</button>
+      </Container>
+      
     </Modal>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -54,4 +54,48 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+    box-shadow: 3px 3px 12px rgba(0, 0, 0, 0.2);
+    outline: none;
+  }
+
+  .react-modal-close {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    outline: none;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
 `
